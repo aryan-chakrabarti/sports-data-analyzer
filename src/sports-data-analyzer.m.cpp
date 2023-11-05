e3d2066fcaffdef126c02af6cd59bf8a146ced7f
Aryan Chakrabarti <aryanc@berkeley.edu>
2023-11-05 01:03:27 -0500

    Add option to take in multiple player names as args

--- src/sports-data-analyzer.m.cpp
+++ src/sports-data-analyzer.m.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "pfrscraper_scraper.h"
 
+std::string getPlayerData(const std::string& player) {
+    pfrscraper::Scraper scraper;
+    return scraper.getPlayerData(player);
+}
+
 int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            std::string playerList(getPlayerData(argv[i]));
+            if (playerList != "") {
+                std::cout << "Got the data for " << argv[i] << "."
+                          << "\n";
+            }
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+
     while (true) {
         std::string player;
         std::cout << "Enter a player's name (type 'q' to quit): ";
@@ -11,13 +26,14 @@ int main(int argc, char** argv) {
         if (player == "q") {
             break;
         }
-        pfrscraper::Scraper scraper;
-        std::string playerList(scraper.getPlayerData(player));
+        std::string playerList(getPlayerData(player));
         if (playerList != "") {
             std::cout << "Got the data for " << player << "."
                       << "\n";
         }
     }
+
     std::cout << std::endl;
+
     return 0;
 }
