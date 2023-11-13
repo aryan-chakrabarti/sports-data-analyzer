a62eb083cd09684c91772181f77ba56f90b9c154
Aryan Chakrabarti <aryanc@berkeley.edu>
2023-11-12 23:41:46 -0500

    Add . as a separator for to_proper

--- src/pfrscraper/pfrscraper_utils.cpp
+++ src/pfrscraper/pfrscraper_utils.cpp
@@ -6,7 +6,7 @@ namespace pfrscraper {
 std::string to_proper(std::string str) {
     bool wasSpace(true);
     for (auto& c : str) {
-        bool isSeparator(c == ' ' || c == '\n' || c == '\t');
+        bool isSeparator(c == ' ' || c == '\n' || c == '\t' || c == '.');
         if (wasSpace && !isSeparator) {
             c = (char)std::toupper(c);
             wasSpace = false;
