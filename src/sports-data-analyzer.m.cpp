b7949f053f736faf9efdfe261f5574eb2c082655
Aryan Chakrabarti <aryanc@berkeley.edu>
2023-11-12 20:57:57 -0500

    Add load, show, and get commands to main

--- src/sports-data-analyzer.m.cpp
+++ src/sports-data-analyzer.m.cpp
@@ -1,35 +1,96 @@
+#include <boost/algorithm/string.hpp>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include "pfrscraper_scraper.h"
 #include "pfrscraper_utils.h"
 
-std::shared_ptr<pfrscraper::DataTableMap<std::string>> getPlayerData(
-    const std::string& player) {
+typedef std::shared_ptr<pfrscraper::DataTableMap<std::string>> PlayerData;
+
+PlayerData getPlayerData(const std::string& player) {
     pfrscraper::Scraper scraper;
     return scraper.getPlayerData(player);
 }
 
-void processPlayer(const std::string& player) {
-    typedef pfrscraper::DataTableMap<std::string> TableMap;
+PlayerData processPlayer(const std::string& player) {
     std::string properName(pfrscraper::to_proper(player));
-    std::cout << "Getting data for " << properName << ":\n\n";
-    std::shared_ptr<TableMap> playerData(getPlayerData(player));
+    std::cout << "Getting data for " << properName << "...\n\n";
+    PlayerData playerData(getPlayerData(player));
     if (!playerData->map().empty()) {
-        std::cout << "Found data for " << properName << ":\n\n";
-        for (auto elem(playerData->map().begin());
-             elem != playerData->map().end(); elem++) {
-            std::cout << elem->second.name() << ":\n";
-            size_t index(0);
-            for (auto row : elem->second.rows()) {
-                std::cout << "\t" << row.first << ":\n";
-                for (auto col : elem->second.columns()) {
-                    std::cout << "\t\t" << col.first << ": "
-                              << *col.second.at(index) << "\n";
-                }
-                index++;
-            }
+        std::cout << "Successfully loaded data for " << properName << "\n";
+    } else {
+        std::cerr << "ERROR: Failed to load data for " << properName
+                  << std::endl;
+    }
+    return playerData;
+}
+
+void printTable(const pfrscraper::DataTable<std::string>& table) {
+    auto rows = table.rows();
+    bool printedCols(false);
+    for (auto row(rows.begin()); row != rows.end(); row++) {
+        if (!printedCols) {
+            std::cout << "\t\t\t";
+            std::for_each(
+                row->second->begin(), row->second->end(),
+                [](auto& elem) { std::cout << "\t\t" << elem.first; });
+            std::cout << "\n";
+            printedCols = true;
         }
+        std::cout << row->first;
+        std::for_each(row->second->begin(), row->second->end(), [](auto& elem) {
+            std::cout << "\t\t\t\t" << elem.second;
+        });
+        std::cout << "\n";
+    }
+    std::cout << std::endl;
+}
+
+void processCommand(const std::string& command, PlayerData& data) {
+    std::vector<std::string> parsedArgs;
+    boost::split(parsedArgs, command, boost::is_any_of("\t "));
+    if (parsedArgs.size() == 0) {
+        return;
+    }
+    std::string function(parsedArgs.at(0));
+    boost::to_lower(function);
+
+    if (function == "load") {
+        std::string joinedArgs(boost::algorithm::join(
+            std::vector<std::string>(parsedArgs.begin() + 1, parsedArgs.end()),
+            " "));
+        PlayerData playerInfo(processPlayer(joinedArgs));
+        if (playerInfo != nullptr && !playerInfo->map().empty()) {
+            data = playerInfo;
+        }
+    } else if (function == "show") {
+        if (data == nullptr || data->map().empty()) {
+            std::cerr << "Cannot show data, no player currently loaded."
+                      << std::endl;
+            return;
+        }
+        std::for_each(
+            data->map().begin(), data->map().end(),
+            [](auto& datapair) { std::cout << datapair.first << "\n"; });
+    } else if (function == "get") {
+        if (data == nullptr || data->map().empty()) {
+            std::cerr << "Cannot show data, no player currently loaded."
+                      << std::endl;
+            return;
+        }
+        if (parsedArgs.size() != 2) {
+            std::cerr << "Please select only one table to show." << std::endl;
+            return;
+        }
+        const auto& dataTable(data->map().find(parsedArgs.at(1)));
+        if (dataTable == data->map().end()) {
+            std::cerr << "Could not find data table with name "
+                      << parsedArgs.at(1) << std::endl;
+            return;
+        }
+        printTable(dataTable->second);
+    } else {
+        std::cerr << "ERROR: Invalid command. Please try again." << std::endl;
     }
 }
 
@@ -48,14 +109,19 @@ int main(int argc, char** argv) {
         std::cout << std::endl;
         return 0;
     } else if (argc == 1) {
+        std::cout
+            << "Welcome to the football player data analyzer.\nStart with the "
+               "'load' command to load a player's info.\nType 'q' to quit.\n";
+        PlayerData data;
         while (true) {
-            std::string player;
-            std::cout << "Enter a player's name (type 'q' to quit): ";
-            std::getline(std::cin, player);
-            if (player == "q") {
+            std::string command;
+            std::cout << "> ";
+            std::getline(std::cin, command);
+            boost::algorithm::trim(command);
+            if (command == "q") {
                 break;
             }
-            processPlayer(player);
+            processCommand(command, data);
         }
 
         std::cout << std::endl;
